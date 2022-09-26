656c48ab2eaf9d2ca1c4f5c58c7845a787c3c20d
Vika <mandarinkavikabakalova@gmail.com>
2022-09-26 20:24:10 +0400

    Задача 52

---
+++ Z52/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое
+// элементов в каждом столбце.
+
+int[,] array = new int[3, 4];
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Среднее арифметическое в каждом столбце:");
+Console.Write(array.GetLength(0));
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"{ sum / array.GetLength(0)} ");
+}
+Console.ReadLine();
