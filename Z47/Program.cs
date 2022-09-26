6ba368eb71be0e6b11db199fccf035f4a874929c
Vika <mandarinkavikabakalova@gmail.com>
2022-09-26 18:17:57 +0400

    Задача47

---
+++ Z47/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный 
+//случайными вещественными числами.
+
+
+void PrintArray(int[,] table)
+{
+for (int i = 0; i < 3; i++) 
+{
+    for (int j = 0; j < 4; j++)  
+    {
+        Console.Write($"{table[i,j]} "); 
+    }
+    Console.WriteLine();
+}
+}
+void FillArray (int[,]table)
+{
+    for (int i = 0; i < 3; i++)  
+{
+    for (int j = 0; j < 4; j++)  
+    {
+        table[i,j]= new Random().Next(-99,99);
+    }
+}
+}
+int [,] table2= new int [3,4];
+FillArray(table2);
+Console.WriteLine();
+PrintArray(table2);
